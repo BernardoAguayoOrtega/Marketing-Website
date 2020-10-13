5faf460d35a568db9af002b4e1b5659bfef7773f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-13 18:40:03 -0500

    install css dependencies for webpacl
    -css loader
    -styles loader

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,11 +5,15 @@ import { mount } from 'enzyme';
 //import app component
 import App from '../components/App';
 
-describe('<App /> rendering', () => {
-  test('test', () => {
-    // eslint-disable-next-line react/jsx-filename-extension
-    const app = mount(<App />);
+describe('<App />', () => {
+  // eslint-disable-next-line react/jsx-filename-extension
+  const app = mount(<App />);
 
+  test('rendering', () => {
     expect(app.length).toEqual(1);
+  });
+
+  test('app text', () => {
+    expect(app.text()).toEqual('Im the app component');
   });
 });
